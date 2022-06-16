999cb73cfa4b29031cdec68d5fdc498581ceaf6b
Normandes Junior <normandes.junior@zup.com.br>
2022-06-16 15:30:52 -0300

    assignment

---
+++ assignment1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, value := range n {
+		if value%2 == 0 {
+			fmt.Printf("%d is even\n", value)
+		} else {
+			fmt.Printf("%d is odd\n", value)
+		}
+	}
+}
